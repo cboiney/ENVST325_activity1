40f80e11c1e503f4ade67491e1583e51844702c0
Cade Boiney <cboiney@hamilton.edu>
2024-09-05 15:44:45 +0000

    r1

--- test.R
+++ test.R
@@ -1,1 +1,21 @@
 print("test")
+
+prontly <- 5
+
+print(prontly)
+
+prontly <- prontly*prontly
+
+prontly = 74
+
+func = c(prontly, prontly+100)
+names = c("one", "prontly2")
+
+mean_v = mean(func)
+
+#style for subsetting vectors
+#peaks[1]
+
+#dataframes
+frame <- data.frame(elev = func, 
+           name = names)
